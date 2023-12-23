085922bd6359a740d3217121e0ff6aca4c95835f
noobie-devil <school0123456@gmail.com>
2023-12-23 11:24:36 +0700

    update admin router

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -16,7 +16,10 @@ import {permissionMiddleware} from "../middlewares/permission.middleware.js";
 const adminRouter = express.Router()
 adminRouter.post('/login', asyncHandler(adminLogin))
 adminRouter.post('/refresh-token', asyncHandler(adminRefreshToken))
+
 adminRouter.use(authentication)
+adminRouter.put('/:adminId/change-password', asyncHandler(changeAdminPassword))
+adminRouter.put('/:adminId/change-username', asyncHandler(changeAdminUsername))
 
 const adminSegmentRouter = express.Router()
 adminSegmentRouter.use(permissionMiddleware)
@@ -24,8 +27,8 @@ adminSegmentRouter.get('/', asyncHandler(getAdmin))
 adminSegmentRouter.post('/', asyncHandler(createAdmin))
 adminSegmentRouter.delete('/:adminId', asyncHandler(deleteAdmin))
 
-adminSegmentRouter.put('/:adminId/change-password', asyncHandler(changeAdminPassword))
-adminSegmentRouter.put('/:adminId/change-username', asyncHandler(changeAdminUsername))
+// adminSegmentRouter.put('/:adminId/change-password', asyncHandler(changeAdminPassword))
+// adminSegmentRouter.put('/:adminId/change-username', asyncHandler(changeAdminUsername))
 
 const adminGroupsRouter = express.Router()
 adminGroupsRouter.use(permissionMiddleware)
@@ -37,6 +40,7 @@ adminGroupsRouter.post('/:groupId/admins', asyncHandler(addToGroup))
 adminGroupsRouter.delete('/:groupId/admins', asyncHandler(removeFromGroup))
 
 adminRouter.use('/admin', adminSegmentRouter)
+
 adminRouter.use('/admin-groups', adminGroupsRouter)
 
 export default adminRouter
